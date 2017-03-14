540eed9c1b4db39ad4440f71343177bb4746222f
Đặng Nguyễn Thế Hưng <dangnguyenthehung@gmail.com>
2017-03-14 15:30:05 +0700

    Add project files.

---
+++ MasterApp/MasterApp.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MasterApp.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new MasterApp.App());
+        }
+    }
+}
